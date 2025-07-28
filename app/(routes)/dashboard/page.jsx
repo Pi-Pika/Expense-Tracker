0784038812b54c2fb043ad567a067ed02d49eb32
Zakia Sultana Nisa <zakianisa970@gmail.com>
2025-07-28 07:07:09 +0600

    Dashboard updated

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,10 +1,13 @@
+"use client"
 import React from 'react'
 import { UserButton } from '@clerk/nextjs'
 
 function Dashboard() {
+  const { user } = useUser();
   return (
-    <div>
-      Dashboard
+    <div className='p-10'>
+      <h2 className='font-bold text-3xl'>Hello, {user?.fullName} 🤘</h2>
+      <p className='text-gray-700'>Are you spending your money wisely? 🤔 Let's manage your expenses! 😊</p>
     </div>
   )
 }
